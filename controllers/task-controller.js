6d2a7912980de359f93dc7949bac327bda72f847
ossasoeun <ossasoeun9@gmail.com>
2023-03-20 17:17:12 +0700

    CRUN Task

--- controllers/task-controller.js
+++ controllers/task-controller.js
@@ -1,13 +1,74 @@
-const getTasks = async (req, res) => {
-  return res.json("Get Task")
+import Task from "../models/task-model.js"
+import JSONStream from "JSONStream"
+
+const getTasks = (req, res) => {
+  const { latest_timestamp } = req.query
+  const { _id } = req.user
+  let query = { owner: _id }
+  if (latest_timestamp) {
+    query.updated_at = { $gte: latest_timestamp, $ne: latest_timestamp }
+  }
+  Task.find(query)
+    .sort({ created_at: -1 })
+    .cursor()
+    .pipe(JSONStream.stringify())
+    .pipe(res.type("json"))
 }
 
 const createTask = async (req, res) => {
-  return res.json("Create Task")
+  const { _id } = req.user
+  const { label, room } = req.body
+
+  if (!label) {
+    return res.status(400).json({ message: "Label is required" })
+  }
+
+  try {
+    const task = await Task.create({
+      owner: _id,
+      label: label,
+      room,
+    })
+    return res.json(task)
+  } catch (error) {
+    return res.status(500).json({ message: error })
+  }
 }
 
 const editTask = async (req, res) => {
-  return res.json("Edit Task")
+  const { _id } = req.user
+  const {
+    label,
+    room,
+    depend_on,
+    start_at,
+    end_at,
+    status,
+    priority,
+    location,
+    note,
+  } = req.body
+
+  try {
+    const task = await Task.updateOne(
+      { _id: req.params.id },
+      {
+        owner: _id,
+        label: label,
+        room,
+        depend_on: depend_on ? JSON.stringify(depend_on) : undefined,
+        start_at,
+        end_at,
+        status,
+        priority,
+        location,
+        note,
+      }
+    )
+    return res.json(task)
+  } catch (error) {
+    return res.status(500).json({ message: error })
+  }
 }
 
 const assignTaskTo = async (req, res) => {
@@ -19,7 +80,12 @@ const removeAssignTaskTo = async (req, res) => {
 }
 
 const deleteTask = async (req, res) => {
-  return res.json("Create Task")
+  try {
+    await Task.deleteOne({ _id: req.params.id, owner: req.user._id })
+    return res.json({ message: "Delete successfully" })
+  } catch (error) {
+    return res.status(500).json({ message: error })
+  }
 }
 
 const addAttachment = async (req, res) => {
@@ -38,5 +104,5 @@ export {
   editTask,
   assignTaskTo,
   removeAssignTaskTo,
-  deleteTask
+  deleteTask,
 }
