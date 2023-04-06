cd9bf7e7a77d806d9a2f93dcaf5e1ccaa974b0eb
Oeng Mengthong <mengthongoenggl@gmail.com>
2023-04-06 12:10:25 +0700

    try new default path

--- controllers/file-controller.js
+++ controllers/file-controller.js
@@ -2,9 +2,8 @@ import path from "path"
 import fs from "fs"
 
 const root = process.cwd()
-const __dirname = "./"
-const defaultImagePath = path.join(__dirname, 'asset-data', 'default.png');
-const glitchSound = path.join(__dirname, 'asset-data', 'glitch-sound.mp3');
+const defaultImagePath = path.join(root, 'storage', 'default/default.png');
+const glitchSound = path.join(root, 'storage', 'default/glitch-sound.mp3');
 
 const userProfilePath = path.join(root, "storage", "user-profile")
 
